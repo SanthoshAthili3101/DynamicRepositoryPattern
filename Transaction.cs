9539c3cf12c612a08d1dfc6e34f433f5ed2ada1d
Santhosh Athili <santhosh.athili@phibonacci.com>
2024-03-04 14:29:12 +0530

    Made some changes for git push

---
+++ Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace DynamicRepositoryPattern
+{// this class need to be there in infrasturcture
+    public class Transaction
+    {
+        public const string TransactionTable = "TransactionTable";
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Gender { get; set; }
+    }
+}
---
+++ TransactionDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DynamicRepositoryPattern
+{
+    // this class i need to create in infrastrucure
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Transaction>(_ =>
+            {
+                _.ToTable(Transaction.TransactionTable);
+                _.HasKey(_ => _.Id);
+
+                _.Property(_ => _.Name).IsRequired();
+
+                _.Property(_ => _.Description).IsRequired();
+
+                _.Property(_ => _.Gender).IsRequired();
+            });
+
+        }
+    }
+}
